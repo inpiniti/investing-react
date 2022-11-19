f86e1847ea46374dc94e483c04031c1eed8c29aa
inpiniti <younginpiniti@gmail.com>
2022-11-19 18:30:07 +0900

    FEAT : 관심 종목 리덕스 Author : inpiniti Date : 2022-11-19

--- src/store/interestingStock.js
+++ src/store/interestingStock.js
@@ -1,15 +1,17 @@
-const POST = "stock/POST";
-const DELETE = "stock/DEL";
+const POST = "interesting/POST";
+const GET = "interesting/GET";
+const DELETE = "interesting/DEL";
 
 export const post = (stock) => ({ type: POST, value: stock });
+export const get = (stock) => ({ type: GET, value: stock });
 export const del = (stock) => ({ type: DELETE, value: stock });
 
 const initialState = [];
 
-export default function selected(state = initialState, action) {
+export default function interesting(state = initialState, action) {
   switch (action.type) {
     case POST:
-      return [...new Set([...state, action.value])];
+      return [...action.value];
      case DELETE:
       return state.filter(value => value !== action.value);
      default:
