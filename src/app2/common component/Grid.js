018c8cf8fc99a4bc3139ae53d588754783651f3d
inpiniti <younginpiniti@gmail.com>
2022-11-19 18:30:50 +0900

    FEAT : 그리드 컴포넌트 확장 Author : inpiniti Date : 2022-11-19
      * 10단계까지 늘림

--- src/app2/common component/Grid.js
+++ src/app2/common component/Grid.js
@@ -6,6 +6,12 @@ export default function Grid({ children, gridType: gridType = 1, className, styl
     2: styles.grid2,
     3: styles.grid3,
     4: styles.grid4,
+    5: styles.grid5,
+    6: styles.grid6,
+    7: styles.grid7,
+    8: styles.grid8,
+    9: styles.grid9,
+    10: styles.grid10,
     layout: styles.layout,
   };
 
