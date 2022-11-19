be791811eb99648f0486840f9eebf93fa59760bd
inpiniti <younginpiniti@gmail.com>
2022-11-19 18:29:00 +0900

    FEAT : 필요없는 컴포넌트 제거 Author : inpiniti Date : 2022-11-19

--- src/app2/component/bottomBar/listOfTrainedModels/ListOfTrainedModels.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ListOfTrainedModels from "./ListOfTrainedModels";
-
-export default {
-  title: "ListOfTrainedModels",
-  component: ListOfTrainedModels,
-};
-
-export const Default = () => <ListOfTrainedModels/>;
--- src/app2/component/bottomBar/listOfTrainedModels/model/Models.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Models from "./Models";
-
-export default {
-  title: "Models",
-  component: Models,
-};
-
-export const Default = () => <Models/>;
