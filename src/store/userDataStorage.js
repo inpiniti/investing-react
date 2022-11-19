c747933783687e79c3bf86497c45504edf888f96
inpiniti <younginpiniti@gmail.com>
2022-11-19 18:25:42 +0900

    FEAT : 브라우저 꺼도 저장할수 있는 async-storage 추가 Author : inpiniti Date : 2022-11-19

---
+++ src/store/userDataStorage.js
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import {post} from "./interestingStock";
+
+const key = 'userData';
+
+const userDataStorage = {
+  async get() {
+    try {
+      const rawData = await AsyncStorage.getItem(key);
+      if (!rawData) {
+        throw new Error('No saved ' + key);
+      }
+      const savedData = JSON.parse(rawData);
+      return savedData;
+    } catch (e) {
+      throw new Error('Failed to load ' + key);
+    }
+  },
+  async set(data) {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      throw new Error('Failed to save ' + key);
+    }
+  },
+};
+
+export default userDataStorage;
