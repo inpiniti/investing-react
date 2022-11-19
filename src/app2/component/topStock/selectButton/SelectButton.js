7376225fc12c3be062dd787e2518bae4c3aa3600
inpiniti <younginpiniti@gmail.com>
2022-11-19 14:17:56 +0900

    FEAT : topStock 패치 구현 Author : inpiniti Date : 2022-11-19
      * 테스트는 아직 안해봄

--- src/app2/component/topStock/selectButton/SelectButton.js
+++ src/app2/component/topStock/selectButton/SelectButton.js
@@ -1,19 +1,34 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
+import {useDispatch} from "react-redux";
+import {post} from "../../../../store/topStock";
+
 import styles from './SelectButton.module.css';
 import {useState} from "react";
+import useFetch from "../../../../hooks/useFetch";
 
 export default function SelectButton() {
   const dropdownList = [
     "네이버 검색상위 종목",
-    "네이버 거래량 급증",
-    "다음 인기검색",
-    "다음 인기토론"
+    "네이버 인기토론",
+    "네이버 인기뉴스",
+  ];
+  const apiList = [
+    "http://113.131.152.55:5000/naver/popular",
+    "http://113.131.152.55:5000/naver/discussion",
+    "http://113.131.152.55:5000/naver/news",
   ];
   const [title, setTitle] = useState(dropdownList[0]);
 
-  const dropdownClick = (index) => setTitle(dropdownList[index]);
+  const dropdownClick = (index) => {
+    setTitle(dropdownList[index]);
+    topStockPost(index);
+  }
+
+  // 패치
+  const dispatch = useDispatch();
+  const topStockPost = (index) => dispatch(post(useFetch(apiList[index])));
 
   return (
     <>
