5b8c56dc57cc51e01bfcf8da06f59d0092373948
inpiniti <younginpiniti@gmail.com>
2022-11-19 18:31:10 +0900

    FEAT : 리덕스 추가 Author : inpiniti Date : 2022-11-19
      * interestingStock 추가함

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import selected from "./selected";
 import allStock from "./allStock";
 import stockDetails from "./stockDetails";
 import topStock from "./topStock";
+import interestingStock from "./interestingStock";
 
 const rootReducer = combineReducers({
   selected,
   allStock,
   stockDetails,
   topStock,
+  interestingStock,
 });
 
 export default rootReducer;
